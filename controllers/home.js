a94737c8c1c741e7a1f04b4de1b497dda8e229c6
gracenarez333 <gracemnarez@gmail.com>
2022-06-01 14:10:33 -0700

    adds delete routes for comments, and comment route updates along with more user auth

--- controllers/home.js
+++ controllers/home.js
@@ -29,7 +29,7 @@ router.get('/browse', async (req, res) => {
         url: url,
         headers: {
           Authorization:
-            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ5VlZpbzFGS3UyTmx5M2NPWGUydUJvcHJmQTBXeFQ1aEdqTHg5NklWNU4xNHU5N0pYUiIsImp0aSI6IjI2YWI1OTNmMTg0YTljZGNjZDNlYmNmZTI3NmRiM2Y5Njc2ZTdjMjk5MThkNDUwZWQ3MGM2ZmRjOTdmMWM4MjAwOTNlNDg4NWRiZTlkMDBkIiwiaWF0IjoxNjU0MTA2OTA5LCJuYmYiOjE2NTQxMDY5MDksImV4cCI6MTY1NDExMDUwOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.gNDyUq5zsO8NdsAFkl1tkYNDMFVydkKn6tXgOhV3TE12hPqzbucTZ2_OU63CgsoHovN48abCOAkTZZDR9tE-lY9GptUXrYgT0W4h8sGVnw6HYEA90wSEeQDK_PHPZcYoFksO-BWJYnDwWI2009e1wTjOhrxOiCLHhdNL8yHp5jDre5d0zc1jDdlKrC6Ut-ww7NJbc4xgnlfHgp4ZpSJMRhBkCmb3AeWin6876hVgEXmLp6l80g4PjEkLAxlUnEbN4srej8vIPV4Gn2QOJYbc2yQgVgQpOiSH6UfNQCNrWpcMshGIOxZIGrBMjQsnulfIx7YB7J8q6KN5j2OsPBSUFQ",
+            "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ5VlZpbzFGS3UyTmx5M2NPWGUydUJvcHJmQTBXeFQ1aEdqTHg5NklWNU4xNHU5N0pYUiIsImp0aSI6ImMzZTJmNWFiNGYwYzA2MGM0MWY0MzEwMTZkOTI5ZDNjNjM0NGYzNTA4MzMzMTlhODFmYWE5YmQ0OThkODRlMjEzNzI4ZDRkZDEzMGI4OTk2IiwiaWF0IjoxNjU0MTE1ODQ1LCJuYmYiOjE2NTQxMTU4NDUsImV4cCI6MTY1NDExOTQ0NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.Su_RMREjpWaYX985JMSUVvSjYNF8q2qe07hvLLNAFO-yFjiBrxEOWcHRasYBFBhMSKB6el0j4AZaHqV0t1lfoVP6cGo4ktQ0_ovLEBrVbnYmzo--oFY8enRsC6VM_MjHc0chQigP4CazeMCdyy8c2A3AzeXcPOa4ciBPpEQ4kfL6Wi3lUxi3sGcgdb_O5HK3COt93zlv5HsGDShKvV_08GuxUyfUfUGC7RD0MW01_8EaZHWube-W8dJpKeR5_TmQa-MW_whT3u-vRX4JQT2XProLGi4-D2WzKhXNsx6H5asqA73VCXyBPbtbjPAI6jX12JZc2qaMRILbzjWaS_9Amg",
         },
       });
       allDogs = response.data.animals.filter(dog => dog.type === 'Dog')
@@ -51,12 +51,56 @@ router.get('/about/:id', async (req, res) => {
             url: url,
             headers: {
                 Authorization:
-                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ5VlZpbzFGS3UyTmx5M2NPWGUydUJvcHJmQTBXeFQ1aEdqTHg5NklWNU4xNHU5N0pYUiIsImp0aSI6IjI2YWI1OTNmMTg0YTljZGNjZDNlYmNmZTI3NmRiM2Y5Njc2ZTdjMjk5MThkNDUwZWQ3MGM2ZmRjOTdmMWM4MjAwOTNlNDg4NWRiZTlkMDBkIiwiaWF0IjoxNjU0MTA2OTA5LCJuYmYiOjE2NTQxMDY5MDksImV4cCI6MTY1NDExMDUwOSwic3ViIjoiIiwic2NvcGVzIjpbXX0.gNDyUq5zsO8NdsAFkl1tkYNDMFVydkKn6tXgOhV3TE12hPqzbucTZ2_OU63CgsoHovN48abCOAkTZZDR9tE-lY9GptUXrYgT0W4h8sGVnw6HYEA90wSEeQDK_PHPZcYoFksO-BWJYnDwWI2009e1wTjOhrxOiCLHhdNL8yHp5jDre5d0zc1jDdlKrC6Ut-ww7NJbc4xgnlfHgp4ZpSJMRhBkCmb3AeWin6876hVgEXmLp6l80g4PjEkLAxlUnEbN4srej8vIPV4Gn2QOJYbc2yQgVgQpOiSH6UfNQCNrWpcMshGIOxZIGrBMjQsnulfIx7YB7J8q6KN5j2OsPBSUFQ",
+                "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJ5VlZpbzFGS3UyTmx5M2NPWGUydUJvcHJmQTBXeFQ1aEdqTHg5NklWNU4xNHU5N0pYUiIsImp0aSI6ImMzZTJmNWFiNGYwYzA2MGM0MWY0MzEwMTZkOTI5ZDNjNjM0NGYzNTA4MzMzMTlhODFmYWE5YmQ0OThkODRlMjEzNzI4ZDRkZDEzMGI4OTk2IiwiaWF0IjoxNjU0MTE1ODQ1LCJuYmYiOjE2NTQxMTU4NDUsImV4cCI6MTY1NDExOTQ0NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.Su_RMREjpWaYX985JMSUVvSjYNF8q2qe07hvLLNAFO-yFjiBrxEOWcHRasYBFBhMSKB6el0j4AZaHqV0t1lfoVP6cGo4ktQ0_ovLEBrVbnYmzo--oFY8enRsC6VM_MjHc0chQigP4CazeMCdyy8c2A3AzeXcPOa4ciBPpEQ4kfL6Wi3lUxi3sGcgdb_O5HK3COt93zlv5HsGDShKvV_08GuxUyfUfUGC7RD0MW01_8EaZHWube-W8dJpKeR5_TmQa-MW_whT3u-vRX4JQT2XProLGi4-D2WzKhXNsx6H5asqA73VCXyBPbtbjPAI6jX12JZc2qaMRILbzjWaS_9Amg",
             },
         });
-        res.render('pages/about', { user: res.locals.user, animal: response.data.animal })
+        const walkComments = await db.comment.findAll({
+            where: {
+                petId: req.params.id
+            },
+            include: [db.user]
+        })
+        console.log(walkComments)
+        res.render('pages/about', { user: res.locals.user, animal: response.data.animal, walkComments })
     } catch(err) {
     console.log('FIRE', err)
+    }
+})
+
+// POST /home/about/:id -- posts comments from annonymous users
+router.post('/about/:id', async (req, res) => {
+    try {
+        await db.comment.create({
+            content: req.body.content,
+            userId: res.locals.user.dataValues.id,
+            petId: req.params.id
+        })
+        res.redirect(`/home/about/${req.params.id}`)
+    } catch (err) {
+        console.log('FIREER', err)
+    }
+})
+
+// DELETE /home/about/:id -- deletes a comment from the list
+router.delete('/about/:id', async (req, res) => {
+    // check if user is authorized
+    if (!res.locals.user) {
+        res.render('users/login', { msg: 'please log in to continue' })
+        return // end the route here
+    }
+    try {
+        // find an instance
+        const instanceComment = await db.comment.findOne({
+            where: {
+                userId: res.locals.user.dataValues.id,
+                id: req.body.id
+            }
+        })
+        console.log(instanceComment)
+        await instanceComment.destroy()
+        res.redirect(`/home/about/${req.params.id}`)
+    } catch (err) {
+        console.log('FIRE', err)
     }
 })
 
@@ -100,6 +144,11 @@ router.post('/favorites', async (req, res) => {
 
 // DELETE /home/favorites -- deletes a favorite from the list
 router.delete('/favorites', async (req, res) => {
+    // check if user is authorized
+    if (!res.locals.user) {
+        res.render('users/login', { msg: 'please log in to continue' })
+        return // end the route here
+    }
     try {
         // find an instance
         const instance = await db.pet.findOne({
